b8e2692413ae0d3f4261d6ec7fa3a9210eb125ca
Zeeshan Ali Anjam <zeeshanalianjam49@gmail.com>
2025-10-09 12:53:42 +0500

    change the type number to string of the duration field

--- server/src/models/tour.model.js
+++ server/src/models/tour.model.js
@@ -4,7 +4,7 @@ const tourSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    duration: { type: Number, required: true }, 
+    duration: { type: String, required: true }, 
     location: { type: String, required: true },
     imageUrl: { type: String, required: true },
     rating: { type: Number },
